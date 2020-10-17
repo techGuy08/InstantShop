772a0445bef60d16a353c82c956df1f2dd5245cb
korro21 <42660934+korro21@users.noreply.github.com>
2020-10-17 15:34:20 +0100

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist/js"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "../index.html",
+      template: "src/index.html",
+    }),
+  ],
+};
